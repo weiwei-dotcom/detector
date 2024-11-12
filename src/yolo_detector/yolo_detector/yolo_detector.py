4bab821dba90ec19a038faa48d768fcbb5294704
weiwei-dotcom <728841808@qq.com>
2024-11-12 21:25:54 +0800

    通过系统默认pip狭窄ultralytics库解决yolo节点库依赖问题，将yolo展示相关参数也放到了camera_node下的config文件中.

--- src/yolo_detector/yolo_detector/yolo_detector.py
+++ src/yolo_detector/yolo_detector/yolo_detector.py
@@ -2,13 +2,8 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-# my_project/main.py
-import sys
-from pathlib import Path
-# 添加 yolov10 源代码目录到 sys.path
-sys.path.append("/home/ww/Documents/yolov10")
-# 导入 YOLOv10 类并创建对象
 from ultralytics import YOLOv10
+import yaml
 import cv2
 
 class YoloDetector(Node):
@@ -17,18 +12,40 @@ class YoloDetector(Node):
     """
     def __init__(self):
         super().__init__('yolo_detector')
-        self.model = YOLOv10('yolov10n.pt')
+        self.model = YOLOv10('/home/wl/Documents/yolov10/yolov10n.pt')
         self.bridge = CvBridge()
         self.image_subscriber = self.create_subscription(Image, 'image', self.image_callback, 10)
+        with open('/home/wl/Documents/detector/src/camera_node/config/config.yaml', 'r', encoding='utf-8') as file:
+            self.config = yaml.safe_load(file)
         
     def image_callback(self, img_msg):
         cv_image = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding='bgr8')
-        results = self.model(cv_image)
-        annotated_frame = results[0].plot() 
-        cv2.imshow('YOLOv10 Object Detection', annotated_frame)
+        results = self.model.predict(cv_image, conf=self.config['confidence'])
+        for result in results:
+            for box in result.boxes:
+                cv2.rectangle(cv_image, 
+                              (int(box.xyxy[0][0]), int(box.xyxy[0][1])), 
+                              (int(box.xyxy[0][2]), int(box.xyxy[0][3])), 
+                              (255, 0, 0), self.config['box_thickness'])
+                cv2.putText(cv_image,
+                            f"{result.names[int(box.cls[0])]}",
+                            (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                            cv2.FONT_HERSHEY_PLAIN,
+                            self.config['font_size'],
+                            (255, 0, 0),
+                            self.config['text_thickness'])
+                cv2.putText(cv_image,
+                            f"({int(box.xyxy[0][0])}, {int(box.xyxy[0][1])})",
+                            (int(box.xyxy[0][0]), int(box.xyxy[0][1]) + 22),
+                            cv2.FONT_HERSHEY_PLAIN,
+                            self.config['font_size'],
+                            (255, 0, 0),
+                            self.config['text_thickness'])
+        cv2.imshow('YOLOv10 Object Detection', cv_image)
+        cv2.waitKey(self.config['camera_fps'])
         
-if __name__ == '__main__':
-    rclpy.init()
+def main(args=None):
+    rclpy.init(args=args)
     yolo_detector = YoloDetector()
     rclpy.spin(yolo_detector)
     rclpy.shutdown()
